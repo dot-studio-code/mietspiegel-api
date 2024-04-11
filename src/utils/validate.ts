fd7472ada76fa75ed99cdd34a66f5cc5e338b3ee
tlmn <mail@tlmn.io>
2024-04-11 09:47:02 +0200

    split into individual schema definitions

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,20 +1,28 @@
 import { z } from "zod";
 import { getDistrictsByZipCode } from "./lib";
 
+const objStreetSchema = z.string().min(1, "obj_street is required.");
+
+const objHouseNumberSchema = z
+  .string()
+  .min(1, "houseNumber must be at least 1 character long")
+  .transform((str) => parseInt(str, 10));
+
+const objHouseNumberSupplementSchema = z
+  .string()
+  .max(1, "obj_houseNumberSupplement cannot be longer than 1 character.")
+  .optional();
+
+const objZipCodeSchema = z
+  .string()
+  .min(5, "obj_zipCode must be at least 5 characters long.")
+  .refine((zipCode) => getDistrictsByZipCode(zipCode).length >= 1, {
+    message: "obj_zipCode must match at least one district.",
+  });
+
 export const residentialStatusSchema = z.object({
-  obj_street: z.string().min(1, "obj_street is required."),
-  obj_houseNumber: z
-    .string()
-    .min(1, "houseNumber must be at least 1 character long")
-    .transform((str) => parseInt(str, 10)),
-  obj_houseNumberSupplement: z
-    .string()
-    .max(1, "obj_houseNumberSupplement cannot be longer than 1 character.")
-    .optional(),
-  obj_zipCode: z
-    .string()
-    .min(5, "obj_zipCode must be at least 5 characters long.")
-    .refine((zipCode) => getDistrictsByZipCode(zipCode).length >= 1, {
-      message: "obj_zipCode must match at least one district.",
-    }),
+  obj_street: objStreetSchema,
+  obj_houseNumber: objHouseNumberSchema,
+  obj_houseNumberSupplement: objHouseNumberSupplementSchema,
+  obj_zipCode: objZipCodeSchema,
 });
