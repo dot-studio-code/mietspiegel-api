40a46056e8582acdf643bd5fb164134e4e088207
tlmn <mail@tlmn.io>
2024-04-11 16:33:48 +0200

    use better-sqlite3 and fix query

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { convertHouseNumber } from "./utils/lib";
-import sqlite3 from "sqlite3";
+import { convertHouseNumber, getDistrictsByZipCode } from "./utils/lib";
+import Database, { Database as DatabaseType } from "better-sqlite3";
 import { residentialStatusSchema } from "./utils/validate";
 
 dotenv.config();
@@ -9,13 +9,14 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const db = new sqlite3.Database("./db/rentIndex.sqlite", (err) => {
-  if (err) {
-    console.error("Error opening database", err);
-  } else {
-    console.log("Connected to the SQLite database.");
-  }
-});
+let db: DatabaseType;
+
+try {
+  db = new Database("./db/rentIndex.sqlite");
+  console.log("Connected to the SQLite database.");
+} catch (err) {
+  console.error("Error opening database", err);
+}
 
 app.get("/residentialStatus", (req: Request, res: Response) => {
   const validationResult = residentialStatusSchema.safeParse(req.query);
@@ -24,23 +25,50 @@ app.get("/residentialStatus", (req: Request, res: Response) => {
     return res.status(400).json({ errors: validationResult.error.issues });
   }
 
-  const { obj_houseNumber, obj_houseNumberSupplement } = validationResult.data;
+  const {
+    obj_houseNumber,
+    obj_houseNumberSupplement,
+    obj_street,
+    obj_zipCode,
+    rentIndexYear,
+  } = validationResult.data;
+
   const houseNumberDecimal = convertHouseNumber(
     obj_houseNumber,
     obj_houseNumberSupplement as string
   );
 
-  res.send({});
+  const districtsMap = getDistrictsByZipCode(obj_zipCode);
+  const districtsString = districtsMap.map((district) => `'${district}'`);
+
+  const sql = `
+  SELECT 
+    *, 
+    (houseNumberEndDecimal - ${houseNumberDecimal.houseNumberDecimal}) AS houseNumberDiff 
+  FROM streetIndex_Berlin_${rentIndexYear}
+  WHERE
+    street = '${obj_street}' AND 
+    district IN (${districtsString}) AND 
+    B IN ('${houseNumberDecimal.houseNumberBlock}', 'F', 'K')
+  GROUP BY id HAVING houseNumberDiff >= 0
+  ORDER BY houseNumberDiff ASC LIMIT 1`;
+
+  try {
+    const stmt = db.prepare(sql);
+    const result = stmt.get();
+    if (!result) res.status(404).json({ error: "No matching data found" });
+
+    res.send(result).status(200);
+  } catch (err) {
+    console.error("Database query error", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 process.on("SIGINT", () => {
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log("Closed the database connection.");
-    process.exit(0);
-  });
+  db.close();
+  console.log("Closed the database connection.");
+  process.exit(0);
 });
 
 app.listen(port, () => {
