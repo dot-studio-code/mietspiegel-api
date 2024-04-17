b383d78b734b84e724c41063a14428fd1e539540
tlmn <mail@tlmn.io>
2024-04-17 14:57:35 +0200

    add db return type

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -2,3 +2,21 @@ export interface HouseNumberDetails {
   houseNumberDecimal: number;
   houseNumberBlock: string;
 }
+
+export type DBResult = {
+  id: number;
+  street: string;
+  district: string;
+  eastWest: string;
+  houseNumberStart: number;
+  houseNumberStartSupplement: string;
+  houseNumberRangeStartDecimal: number;
+  houseNumberEnd: number;
+  houseNumberEndSupplement: string;
+  houseNumberRangeEndDecimal: number;
+  B: string;
+  objectStatus: 0 | 1 | 2;
+  noiseLevel: "true" | "false";
+  residentialSituation: "D" | "Z";
+  houseNumberRangeDiff: number;
+};
