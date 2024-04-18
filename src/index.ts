be3412e5d88b853718ac39d004169d2adb5db603
tlmn <mail@tlmn.io>
2024-04-18 18:56:49 +0200

    remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import {
   convertBooleanString,
-  convertHouseNumber,
-  getDistrictsByZipCode,
   getResidentialStatus,
   parseHouseNumberDecimal,
 } from "./utils/lib";
